50a8f085d7a7b910fb2768eb1ae1b1c8ad1c465c
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-12-05 23:31:25 +0530

    admin login added

---
+++ apps/web/app/api/(auth)/adminauth/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse,NextRequest } from "next/server";
+import Admin from "../../../../models/Admin";
+import setConnectionRedis from "../../../../middleware/connectRedisClient";
+import { sendAdminOtpEmail } from "../../../../email/sendAdminOtpEmail";
+export const POST = async(req:NextRequest)=>{
+    try{
+        const data =  await req.json();
+        const redisClient = setConnectionRedis();
+        if(data.type=="createAdmin"){
+            //add auth here
+
+        //check admin exist or not
+        const findAdmin = await Admin.findOne({email:data.email});
+        if(findAdmin){
+            return NextResponse.json({message:"Admin with this email already exists",success:false})
+        }
+        const newAdmin = new Admin(data);
+        await newAdmin.save();
+        return NextResponse.json({message:"Admin created successfully",data:newAdmin,success:true});
+        }
+        else{
+            const findAdmin = await Admin.findOne({email:data.email});
+            if(!findAdmin){
+                return NextResponse.json({message:"Admin not found",success:false})
+            }
+            const otp = Math.floor(100000 + Math.random() * 900000).toString();
+            await redisClient.set(`admin-otp-${data.email}`, otp, "EX", 300); // OTP valid for 5 minutes
+            console.log("Generated OTP:", otp);
+            await sendAdminOtpEmail({ email: data.email, otp ,name:findAdmin.name});
+            return NextResponse.json({message:"OTP sent successfully",success:true})
+
+        }
+    }
+    catch(err){
+        console.error("Error in admin auth route:", err);
+        return NextResponse.json({message:"something went wrong please try again after some time",success:false})
+    }
+}
---
+++ apps/web/app/api/(auth)/adminauth/validate/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse,NextRequest } from "next/server";
+import Admin from "../../../../../models/Admin";
+import jwt from "jsonwebtoken";
+import setConnectionRedis from "../../../../../middleware/connectRedisClient";
+export const POST = async(req:NextRequest)=>{
+    try{
+        const data =  await req.json();
+        const redisClient = setConnectionRedis();
+        const storedOtp = await redisClient.get(`admin-otp-${data.email}`);
+        if(storedOtp!==data.otp){
+            return NextResponse.json({message:"Invalid or expired OTP",success:false})
+        }
+        const admin = await Admin.findOne({email:data.email});
+        if(!admin){
+            return NextResponse.json({message:"Admin not found",success:false})
+        }
+        const token = jwt.sign({ id: admin._id, email: admin.email ,name: admin.name}, process.env.JWT_SECRET!);
+        return NextResponse.json({message:"OTP validated successfully", token, success:true});
+    }
+    catch(err){
+        console.error("Error in admin OTP validation route:", err);
+        return NextResponse.json({message:"something went wrong please try again after some time",success:false})
+    }
+}
