92dd6be81f2f2ab62b60db923dd646c6bc476553
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-12-07 15:35:09 +0530

    verify endpoint added

--- apps/web/app/api/(auth)/verify/route.ts
+++ apps/web/app/api/(auth)/verify/route.ts
@@ -4,6 +4,9 @@ import { NextResponse,NextRequest } from "next/server";
 import Admin from "../../../../models/Admin";
 import { verifyAdminToken } from "../../../../utils/verifyToken";
 import { cookies } from "next/headers";
+import StateOfficer from "../../../../models/StateOfficer";
+import Bank from "../../../../models/Bank";
+import User from "../../../../models/User";
 export const GET = async()=>{
     try{
         const cookieStore = await cookies();
@@ -18,10 +21,22 @@ export const GET = async()=>{
             return NextResponse.json({message:"Invalid token",success:false})
         }
         if(data.data?.type=="admin"){
-            const admin = await Admin.findById(data.data?.id);
+            const admin = await Admin.findById({_id:data?.data?.id});
             return NextResponse.json({message:"Admin fetched successfully",data:admin,success:true,type:"admin"})
         }
-        return NextResponse.json({message:"Unauthorized access",success:false})
+        else if(data.data?.type=="stateOfficer"){
+            const stateOfficer = await StateOfficer.findById({_id:data?.data?.id});
+            return NextResponse.json({message:"State Officer fetched successfully",data:stateOfficer,success:true,type:"stateOfficer"})
+        }
+        else if(data.data?.type=="bank"){
+            const bank = await Bank.findById({_id:data?.data?.id});
+            return NextResponse.json({message:"Bank fetched successfully",data:bank,success:true,type:"bank"})
+        }
+        else if(data.data?.type=="user"){
+            const user = await User.findById({_id:data?.data?.id});
+            return NextResponse.json({message:"User access verified",success:true,type:"user"})
+        }
+        return NextResponse.json({message:"Unauthorized access or no method allowed for this",success:false})
     }
     catch(err){
         return NextResponse.json({message:"something went wrong please try again after some time",success:false})
